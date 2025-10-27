1662d174e29fbc34803c09c9b98355178539454a
alevilla23 <sergiovr_2304@icloud.com>
2025-10-26 21:17:17 -0600

    db a mongo

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI 
 export const connectDB = async () => {
   try{
     await mongoose.connect(MONGO_URI)
