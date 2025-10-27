b3c0332d4b6dc7602cd3548c9eec57af445f5f4d
alevilla23 <sergiovr_2304@icloud.com>
2025-10-27 12:08:49 -0600

    endpoints

--- index.js
+++ index.js
@@ -79,6 +79,36 @@ app.get("/getCard/:id", async (req, res) => {
   }
 });
 
+app.get("/endpoints",(req,res)=>{
+  const template = [
+    {
+      path:"https://dispinteligentes9b.onrender.com/getAllCards",
+      method:"GET",
+      description:"trae todas las cartas"
+    },
+    {
+      path:"https://dispinteligentes9b.onrender.com/getCard/:id",
+      method:"GET",
+      description:"Trae cartas por id"
+    },
+    {
+      path:"https://dispinteligentes9b.onrender.com/createCard",
+      method:"POST",
+      description:"Crea la carta"
+    },
+    {
+      path:"https://dispinteligentes9b.onrender.com/updateCard/:id",
+      method:"PUT",
+      description:"Actualiza las cartas parcial o completamente"
+    },
+    {
+      path:"https://dispinteligentes9b.onrender.com/deleteCard/:id",
+      method:"DELETE",
+      description:"Borra una carta"
+    }
+  ];
+  res.json(template)
+})
 // app.post("/send", (req, res) => {
 //   const { user, email } = req.body;
 //   console.log("Datos recibidos:" + user + "" + email);
