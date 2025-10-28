25f9e0d81f2b35e6c862665f24fde3317c434ab1
anasofiav04 <anasofiav04@gmail.com>
2025-10-27 20:51:42 -0600

    sofias comment

--- models/Cards.js
+++ models/Cards.js
@@ -1,25 +1,27 @@
 import mongoose from "mongoose";
 
-const cardSchema = new mongoose.Schema({
-    name :{
-        type: String,
-        trim: true,
-        required: true
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      // podrías agregar una validación o longitud mínima para el nombre :)
     },
-    link:{
-        type: String,
-        trim: true,
-        required: true 
+    link: {
+      type: String,
+      trim: true,
+      required: true,
     },
-    description:{
-          type: String,
-        trim: true,
-        required: true
+    description: {
+      type: String,
+      trim: true,
+      required: true,
     },
-},
-{
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
 export const Card = mongoose.model("Card", cardSchema);
